f117b0685e70ba0080aa717f72c7824cd1ea2c12
Wilton Martins <65777869+wiltonm@users.noreply.github.com>
2020-11-04 14:35:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+            document.querySelectorAll('[wm-folder]').forEach(folder =>{
+                folder.onclick = function(e) {
+                    const ul = folder.nextElementSibling
+                    const d = ul.style.display
+                    ul.style.display = d === 'none' ? 'block' : 'none'
+                }
+            })
+        </script>
